c27d658bfd7841e1be506cd619f7d12436ddc685
CodeKnight314 <tang.ri@northeastern.edu>
2025-05-21 19:22:34 -0700

    Update main.py to handle PPoEnv and DQNEnv

--- src/main.py
+++ src/main.py
@@ -1,26 +1,48 @@
 import argparse
-from src.env import GameEnv
+import os
+from src.env import PPOEnv, DQNEnv
 
-def main(args):
-    env = GameEnv(args.c, "ALE/Pong-v5", args.w, args.num_envs, args.resume_step)
-    if args.train:
-        try:
-            env.train(args.o)
-            env.test(args.o)
-            env.plot_history(args.o)
-        except KeyboardInterrupt:
-            env.plot_history(args.o)
-    else: 
-        env.test(args.o)
+def parse_args():
+    parser = argparse.ArgumentParser(description='Train RL agents on Atari games')
+    parser.add_argument('--config', type=str, required=True, help='Path to config file')
+    parser.add_argument('--algo', type=str, required=True, choices=['ppo', 'dqn'], help='Algorithm to use')
+    parser.add_argument('--env', type=str, default='ALE/Pong-v5', help='Environment name')
+    parser.add_argument('--num_envs', type=int, default=1, help='Number of parallel environments')
+    parser.add_argument('--resume', type=str, default=None, help='Path to checkpoint to resume from')
+    parser.add_argument('--resume_step', type=int, default=0, help='Step to resume from')
+    parser.add_argument('--mode', type=str, default='train', choices=['train', 'test'], help='Mode to run in')
     
-if __name__ == "__main__":
-    parser = argparse.ArgumentParser()
-    parser.add_argument("--c", type=str, required=True, help="config file path")
-    parser.add_argument("--w", type=str, help="path to model weights")
-    parser.add_argument("--o", type=str, required=True, help="output path to save weights and simulations")
-    parser.add_argument("--num_envs", type=int, default=8, help="Number of environments to run for parallel")
-    parser.add_argument("--train", action="store_true", help="flag to toggle training")
-    parser.add_argument("--resume_step", type=int, default=0, help="if no checkpoint file is given, start from this global step number")
-    args = parser.parse_args()
+    return parser.parse_args()
+
+def main():
+    args = parse_args()
     
-    main(args)
+    results_dir = os.path.join('results', args.algo)
+    os.makedirs(results_dir, exist_ok=True)
+    
+    if args.algo == 'ppo':
+        env = PPOEnv(
+            config=args.config,
+            env_name=args.env,
+            weight_path=args.resume,
+            num_envs=args.num_envs,
+            resume_step=args.resume_step
+        )
+    else:
+        env = DQNEnv(
+            config=args.config,
+            env_name=args.env,
+            weight_path=args.resume,
+            num_envs=args.num_envs,
+            resume_step=args.resume_step
+        )
+    
+
+    if args.mode == 'train':
+        env.train(results_dir)
+        env.plot_history(results_dir)
+    else:
+        env.test(results_dir)
+
+if __name__ == '__main__':
+    main() 
